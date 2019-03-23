8a52a7274e7acebb01dd447bf971e1675af1a1f4
Jeff Keating <jkeatinco@gmail.com>
2019-03-23 16:41:30 -0700

    trying innerHTML instead

--- app/static/client.js
+++ app/static/client.js
@@ -27,13 +27,13 @@ function analyze() {
             $('#exampleModal').modal('show');
             if(`${response['result']}` == 'knights') {
                 el('result-label').innerHTML = `Vegas Golden Knight!`;
-                $('#image-picked').clone().appendTo('#result-img');
+                $('#image-picked').clone().innerHTML('#result-img');
                 
                 
             }
             else if(`${response['result']}` == 'notknights'){
                 el('result-label').innerHTML = `Not a Vegas Golden Knight!`; 
-                $('#image-picked').clone().appendTo('#result-img');
+                $('#image-picked').clone().innerHTML('#result-img');
                  
             }
         }
